356a947371299ab0ee71e588d356134e5ab645ee
Maya Stucky <mayaatstucky@gmail.com>
2020-08-06 19:10:37 -0400

    initial starter app built

---
+++ client/src/containers/NoMatch/NoMatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <h1>You shouldn't be here!</h1>
+        </div>
+    );
+}
+
+export default NoMatch;
